83c89feb2a5ac8cc1e5e8cbd1c532aa9abe8faec
Lumosity23 <timochazot00@gmail.com>
2025-10-04 17:17:42 +0200

    debut de l'ajoute de notre librairie pour le servo mteur et creation de l'interuptino et de la fonction d'initialisation

--- defi_info_elec/defi_info_elec.ino
+++ defi_info_elec/defi_info_elec.ino
@@ -1,8 +1,10 @@
 #include <math.h>
-#include <Servo.h>
-Servo Servomoteur1;
+//#include <Servo.h>
+//Servo Servomoteur1;
 const uint8_t pin_servo = 9, pin_RED, pin_GREEN, pin_SWITCH = A2, pin_Y_axes = A0, pin_X_axes = A1, resolution_ADC = 10;
 uint8_t angle_randome = 70;
+long randAngle;
+volatile bool initPartie = false;
 uint16_t mesure_axe_X = 0;
 
 
@@ -18,6 +20,7 @@ void setup()
 
   //Servomoteur1.attach(pin_servo);
   //Servomoteur1.write(0);
+  attachInterrupt(digitalPinToInterrupt(pin_SWITCH, BoutonInit, LOW));
 }
 
 uint8_t lecture_joytick(const uint8_t pin, uint8_t resolution, uint8_t mesure)
@@ -35,6 +38,8 @@ uint8_t lecture_joytick(const uint8_t pin, uint8_t resolution, uint8_t mesure)
   }
   return 0;
 }
+
+
 void loop() 
 {
   mesure_axe_X = analogRead(pin_X_axes);
@@ -47,5 +52,30 @@ void loop()
   case 2:Serial.println("--");   break;
   default: Serial.println("rien");  break;
   }*/
+
+  if (initPartie) {
+    initPartie = false;   // on remet à false l'initialisation
+    init(); // on exécute la fonction init()
+  }
   delay(20);
+}
+
+void init()
+{
+  randAngle = random(181); //reneration d'un nombre entre 0 et 180 (0 et max-1)
+  servoMoteur(90);
+  digitalWrite(pin_GREEN, 1);
+}
+
+
+void servoMoteur(int angleServo)
+{
+  u_int32_t times_ms;
+  u_int32_t temps;
+  times_ms = millis();
+  temps = ((angleServo/180)+1);  // convertir l'anger recu en un temps en "ms" pour faire touner le servo
+  if (millis >= times_ms + temps){  // repeter tout les 20ms
+    digitalWrite(pin_servo, 1);
+    times_ms = millis();
+  }
 }
