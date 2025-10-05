3b8d73d42d25f5c0d5ef0e1247aec6af49d73ed6
laflecherapide <laflecherapide@hotmail.com>
2025-10-05 14:50:42 +0200

    ajout commentaire

--- defi_info_elec/defi_info_elec.ino
+++ defi_info_elec/defi_info_elec.ino
@@ -1,9 +1,20 @@
+/*
+defi final d'info elec bac 1 ecam
+le projet comprends les 4 éléments de la liste suivant : servo moteur, une led RGB, un joystick et parallélisme(millis)
+il tourne sur arduino uno
+le but est de commander un servo moteur à l'aide du joystick pour que l'angle du servo moteur corresponde à un angle choisit aléatoirement. Une led RGB nous indiques si on se rapproche de l'angle ou pas grâce au code couleur suivant :
+-rouge éloigné
+-vert rapproché
+-bleu gagné
+une fois le servo moteur dans le bon angle pendant un certain temps, le servo se place à 90° (position inital), un nouvel angle est choisit et une nouvelle partie commence. Nous avons modifier le servo moteur en soudant un fil sur le potentiomètre pour connaître l'angle effectif du servomoteur pour comparer non pas la commande du servo mais l'angle réel.
+*/
+
 #include <math.h>
-//#include <Servo.h>
-//Servo Servomoteur1;
+#include <Servo.h>
+Servo Servomoteur1;
 const uint8_t pin_servo = 9, pin_RED, pin_GREEN, pin_SWITCH = A2, pin_Y_axes = A0, pin_X_axes = A1, resolution_ADC = 10;
-uint8_t angle_randome = 70;
-long randAngle;
+
+long angle_random;//pk un long
 volatile bool initPartie = false;
 uint16_t mesure_axe_X = 0;
 
@@ -18,15 +29,15 @@ void setup()
   pinMode(pin_GREEN, OUTPUT);
   pinMode(pin_RED, OUTPUT);
 
-  //Servomoteur1.attach(pin_servo);
-  //Servomoteur1.write(0);
+  Servomoteur1.attach(pin_servo);
+  Servomoteur1.write(0);
   attachInterrupt(digitalPinToInterrupt(pin_SWITCH, BoutonInit, LOW));
 }
 
-uint8_t lecture_joytick(const uint8_t pin, uint8_t resolution, uint8_t mesure)
+uint8_t lecture_joytick(const uint8_t pin, uint8_t resolution, uint8_t mesure)//cette fonction est faite pour lire le joystick et renvoyer une valeur quand le joystick est trop penché (d'un coté comme de l'autre) 
 {
   uint8_t limite_sup, limite_inf;
-  double val_max = pow(2, resolution);
+  double val_max = pow(2, resolution);//c'est un double car la fonction pow renvoit un double
   limite_sup = 0.75 * val_max;
   limite_inf = 0.25 * val_max;
   if (mesure >= limite_sup)
@@ -60,15 +71,15 @@ void loop()
   delay(20);
 }
 
-void init()
+void init()//pourquoi faire une fonction init ?
 {
-  randAngle = random(181); //reneration d'un nombre entre 0 et 180 (0 et max-1)
+  angle_random = random(181); //reneration d'un nombre entre 0 et 180 (0 et max-1)
   servoMoteur(90);
   digitalWrite(pin_GREEN, 1);
 }
 
 
-void servoMoteur(int angleServo)
+void servoMoteur(int angleServo)//on va faire la librairie plus tard, voir ne pas la faire car il faut une interruption timer
 {
   u_int32_t times_ms;
   u_int32_t temps;
